31e72bcaab4108fae259c999df2e172448e4592a
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-08-14 12:37:23 +0300

    create astro_objects controller

---
+++ app/helpers/astro_objects_helper.rb
@@ -0,0 +1,2 @@
+module AstroObjectsHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'astro_objects#index'
+
+  resources :astro_objects
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
