a2071312cf60345c064529473c531971d11df626
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-08-14 15:53:46 +0300

    add new astro object implemented

--- app/controllers/astro_objects_controller.rb
+++ app/controllers/astro_objects_controller.rb
@@ -7,6 +7,20 @@ class AstroObjectsController < ApplicationController
   end
 
   def create
+    @astro_object=AstroObject.new(astro_object_params)
+    if @astro_object.save
+      render action: 'create'
+    else
+      render action: 'new'
+    end
   end
-  
+
+  private
+
+  # разрешение передачи параметров
+  def astro_object_params
+    params.require(:astro_object).permit(:name, :date, :time, :comment)
+  end
+
+
 end
