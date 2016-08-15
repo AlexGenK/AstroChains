48600feb46d19dc660e7af6f9eb4c2433f064b92
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-08-15 08:24:40 +0300

    add delete/update actions for AstroObject

--- app/controllers/astro_objects_controller.rb
+++ app/controllers/astro_objects_controller.rb
@@ -20,6 +20,25 @@ class AstroObjectsController < ApplicationController
     @astro_object=AstroObject.find(params[:id])
   end
 
+  def edit
+    @astro_object=AstroObject.find(params[:id])
+  end
+
+  def update
+    @astro_object=AstroObject.find(params[:id])
+
+    if @astro_object.update(astro_object_params)
+      redirect_to @astro_object
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    AstroObject.find(params[:id]).destroy
+    redirect_to astro_objects_path
+  end
+
   private
 
   # разрешение передачи параметров
