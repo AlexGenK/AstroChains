a8f67dedcfcb49738165e1ad53ba05d2652555b8
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-08-15 20:10:56 +0300

    create chains controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   get 'about'=>'single_pages#about'
 
-  resources :astro_objects
+  resources :astro_objects do
+    resources :chains
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
