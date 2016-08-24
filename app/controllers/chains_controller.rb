1432f2b71f6a8f960728a38476210b01b43d55d7
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-08-24 10:17:46 +0300

    add preview button in new form and fix septener error

--- app/controllers/chains_controller.rb
+++ app/controllers/chains_controller.rb
@@ -2,14 +2,22 @@ class ChainsController < ApplicationController
 
   def new
     @astro_object=AstroObject.find(params[:astro_object_id])
+    @chain=@astro_object.chains.build
   end
 
   def create
+
     @astro_object=AstroObject.find(params[:astro_object_id])
     @chain=@astro_object.chains.new(chain_params)
     @chain.code=params.to_s
-    @chain.save
-    redirect_to @astro_object
+
+    if params[:commit]=='Просмотреть'
+      render action: 'new'
+    else
+      @chain.save
+      redirect_to @astro_object
+    end
+
   end
 
   def destroy
@@ -21,7 +29,7 @@ class ChainsController < ApplicationController
 
   # разрешение передачи параметров
   def chain_params
-    params.require(:chain).permit(:kind, :code, :image, :comment)
+    params.require(:chain).permit(:kind, :code, :image, :comment, :septener)
   end
   
 end
