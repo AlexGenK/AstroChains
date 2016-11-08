9450443d482514c2a7382be78dd1a36f6c7a048e
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-11-08 17:07:30 +0200

    work with all end planets

--- app/models/chain.rb
+++ app/models/chain.rb
@@ -1,7 +1,7 @@
 # модель - цепочка диспозиций построенная для какого-либо объекта
 class Chain < ActiveRecord::Base
 
-  # константа в кторой хранятся названия планет, соответствующие им префиксы в модели цепочки
+  # константа в которой хранятся названия планет, соответствующие им префиксы в модели цепочки
   # и символ которым они кодируются в специальном шрифте 'astro'
   PLANETS=[ {:planet_name=>'Солнце', :planet_prefix=>'sun', :planet_symbol=>'Q'},
             {:planet_name=>'Луна', :planet_prefix=>'moo', :planet_symbol=>'R'},
@@ -13,8 +13,8 @@ class Chain < ActiveRecord::Base
             {:planet_name=>'Уран', :planet_prefix=>'ura', :planet_symbol=>'X'},
             {:planet_name=>'Нептун', :planet_prefix=>'nep', :planet_symbol=>'Y'},
             {:planet_name=>'Плутон', :planet_prefix=>'plu', :planet_symbol=>'Z'},
-            {:planet_name=>'Юж. центр', :planet_prefix=>'smn', :planet_symbol=>'&lt;'},
-            {:planet_name=>'Сев. центр', :planet_prefix=>'nmn', :planet_symbol=>'&gt;'},
+            {:planet_name=>'Юж. центр', :planet_prefix=>'smn', :planet_symbol=>'m'},
+            {:planet_name=>'Сев. центр', :planet_prefix=>'nmn', :planet_symbol=>'M'},
             {:planet_name=>'Хирон', :planet_prefix=>'chi', :planet_symbol=>'e'},
             {:planet_name=>'Прозерпина', :planet_prefix=>'pro', :planet_symbol=>'d'},
             ].freeze
@@ -29,7 +29,7 @@ class Chain < ActiveRecord::Base
     graph_nodes=[]
     
     # если цепочка строится по септенеру, количество планет ограничивается семью. иначе - девять
-    nodes_count = septener ? 6 : 9
+    nodes_count = septener ? 6 : 13
     
     # в зависимости от выбранного стиля визуализации центров, определяется цвет рамки, выделяющей центры,
     # и цвет элементов находящихся в центрах
@@ -49,10 +49,6 @@ class Chain < ActiveRecord::Base
         eval("@c#{pl_center}")["color"]=frame_color
       end 
     end
-    if end_center>0
-      instance_variable_set("@c#{end_center}", g.add_graph("cluster#{end_center}"))
-      eval("@c#{end_center}")["color"]=frame_color
-    end
 
     # проходим цикл по всем планетам для формирования узлов графа
     0.upto nodes_count do |i|
@@ -61,37 +57,26 @@ class Chain < ActiveRecord::Base
       pl_symbol=PLANETS[i][:planet_symbol]
       pl_weigth=eval("#{pl_prefix}_weigth")
       pl_retro=eval("#{pl_prefix}_retro")
+      # если планета в основных - создаем узел сразу. если нет, то проверяем признак наличия
+      if (i<=9) || (eval("#{pl_prefix}_exist"))
+        # формируем строку, которая в описании узла-планеты в графе отвечает за вес
+        pl_weigth_string = pl_weigth ? "<font color='forestgreen' point-size='20'>#{pl_weigth}</font>" : ''
 
-      # формируем строку, которая в описании узла-планеты в графе отвечает за вес
-      pl_weigth_string = pl_weigth ? "<font color='forestgreen' point-size='20'>#{pl_weigth}</font>" : ''
+        # формируем строку, которая в описании узла-планеты в графе отвечает за ретроградность
+        pl_retro_string = pl_retro ? "<font color='black' point-size='20'>N</font>" : ''
 
-      # формируем строку, которая в описании узла-планеты в графе отвечает за ретроградность
-      pl_retro_string = pl_retro ? "<font color='black' point-size='20'>N</font>" : ''
-
-      # узнаем, принадлежит ли планета к какому-либо кластеру и добавляем узел-планету к соответствующему кластеру
-      pl_center=eval("#{pl_prefix}_center")
-      case pl_center
-      when 0
-        graph_nodes[i]=g.add_nodes(pl_prefix, :label=>"<<font face='astro-semibold' point-size='25'>#{pl_symbol}#{pl_retro_string}#{pl_weigth_string}</font>>")
-      when 1..6
-        graph_nodes[i]=eval("@c#{pl_center}").add_nodes(pl_prefix, :label=>"<<font face='astro-semibold' point-size='25'>#{pl_symbol}#{pl_retro_string}#{pl_weigth_string}</font>>", :color=>element_color)
+        # узнаем, принадлежит ли планета к какому-либо кластеру и добавляем узел-планету к соответствующему кластеру
+        pl_center=eval("#{pl_prefix}_center")
+        case pl_center
+        when 0
+          graph_nodes[i]=g.add_nodes(pl_prefix, :label=>"<<font face='astro-semibold' point-size='25'>#{pl_symbol}#{pl_retro_string}#{pl_weigth_string}</font>>")
+        when 1..6
+          graph_nodes[i]=eval("@c#{pl_center}").add_nodes(pl_prefix, :label=>"<<font face='astro-semibold' point-size='25'>#{pl_symbol}#{pl_retro_string}#{pl_weigth_string}</font>>", :color=>element_color)
+        end
       end
     end
-
-    # добавляем конечную планету
-    if end_planet>100
-      
-      end_weigth_string = end_weigth ? "<font color='forestgreen' point-size='20'>#{end_weigth}</font>" : ''
-      end_retro_string = end_retro ? "<font color='black' point-size='20'>N</font>" : ''
-
-      case end_center
-      when 0
-        graph_nodes[nodes_count+1]=g.add_nodes('ender', :label=>"<<font face='astro-semibold' point-size='25'>#{ENDERS[end_planet-101]}#{end_retro_string}#{end_weigth_string}</font>>")
-      when 1..6
-        graph_nodes[nodes_count+1]=eval("@c#{end_center}").add_nodes('ender', :label=>"<<font face='astro-semibold' point-size='25'>#{ENDERS[end_planet-101]}#{end_retro_string}#{end_weigth_string}</font>>", :color=>element_color)
-      end
-    end
-
+    
+    nodes_count = septener ? 6 : 9
     # снова проходим по всем планетам для создания связей между узлами графа
     0.upto nodes_count do |i|
       # определяем с какой планетой связана планета
@@ -102,10 +87,8 @@ class Chain < ActiveRecord::Base
       else
         edge_color="black"
       end
-      # создание связи к обычной планете
+      # создание связи
       g.add_edges(graph_nodes[i], graph_nodes[pl_relation], :color=>edge_color, :len=>"1.2") if pl_relation<100
-      # создание свзяи к конечной планете
-      g.add_edges(graph_nodes[i], graph_nodes[nodes_count+1], :color=>edge_color, :len=>"1.2") if (pl_relation>100) && (end_planet>100)
     end
     
     # собственно визуализация графа в файл
