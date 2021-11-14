86b7fc180938e13eaa3d689cccffe76b4dd74040
Oleksii Kozhyn <suburbakskin@gmail.com>
2021-11-14 21:04:50 +0300

    Deleted unused paths and methods

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
   root 'astro_objects#index'
 
   get 'about'=>'single_pages#about'
-  get 'libraries'=>'single_pages#libraries'
-  post 'libraries'=>'single_pages#addlibraries'
-  delete 'libraries'=>'single_pages#dellibraries'
 
   resources :astro_objects do
     resources :chains
